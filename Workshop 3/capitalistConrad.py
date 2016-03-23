b50a424e9bb6f68e36495c02afadef6257941c2b
Draga <draga.doncilapop@my.jcu.edu.au>
2016-03-23 12:52:14 +1000

    Final test commits, added format_currency() function to capitalistConrad Workshop 3

--- Workshop 3/capitalistConrad.py
+++ Workshop 3/capitalistConrad.py
@@ -5,24 +5,30 @@ MAX_DECREASE = 0.05 # 5%
 MIN_PRICE = 1.0
 MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
+def main():
+    price = INITIAL_PRICE
+    print("Starting price:", format_currency(price))
+    day_count = 0
+    while price >= MIN_PRICE and price <= MAX_PRICE:
 
-price = INITIAL_PRICE
-print("Starting price: ${:,.2f}".format(price))
-day_count = 0
-while price >= MIN_PRICE and price <= MAX_PRICE:
+        priceChange = 0
+        # generate a random integer of 1 or 2
+        # if it's 1, the price increases, otherwise it decreases
+        if random.randint(1, 2) == 1:
+        # generate a random floating-point number
+        # between 0 and MAX_INCREASE
+            priceChange = random.uniform(0, MAX_INCREASE)
+        else:
+        # generate a random floating-point number
+        # between negative MAX_INCREASE and 0
+            priceChange = random.uniform(-MAX_DECREASE, 0)
 
-    priceChange = 0
-    # generate a random integer of 1 or 2
-    # if it's 1, the price increases, otherwise it decreases
-    if random.randint(1, 2) == 1:
-    # generate a random floating-point number
-    # between 0 and MAX_INCREASE
-        priceChange = random.uniform(0, MAX_INCREASE)
-    else:
-    # generate a random floating-point number
-    # between negative MAX_INCREASE and 0
-        priceChange = random.uniform(-MAX_DECREASE, 0)
+        price *= (1 + priceChange)
+        day_count += 1
+        print("On day {} price is:".format(day_count), format_currency(price))
 
-    price *= (1 + priceChange)
-    day_count += 1
-    print("On day {} price is: ${:,.2f}".format(day_count, price))
+
+def format_currency(value):
+    currency_str =  "${:,.2f}".format(value)
+    return currency_str
+main()
